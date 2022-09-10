12422c9dbc97f2c22b4e2acb26d3cd0fef8882f7
Timmm005 <66874161+Timmm005@users.noreply.github.com>
2022-09-10 14:14:13 +0300

    updated main.cpp to ask for a recommended book instead of a favorite film

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Input your name, surname and favorite film" << "\n";
-    string name, surname, film;
-    cin >> name >> surname >> film;
+    cout << "Input your name, surname and a book that you would like to recommend" << "\n";
+    string name, surname, book;
+    cin >> name >> surname >> book;
 
     ofstream f("info.txt");
-    f << name << " " << surname << " " << film << "\n";
+    f << name << " " << surname << " " << book << "\n";
 }
 
 
