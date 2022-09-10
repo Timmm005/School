24f0ecbba590f88922036da44cb1f7d2aaf1a7bd
Timmm005 <66874161+Timmm005@users.noreply.github.com>
2022-09-10 14:34:40 +0300

    added a 4th question (what political party the user is planning to vote for)

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Input your name, surname and a book that you would like to recommend" << "\n";
-    string name, surname, book;
-    cin >> name >> surname >> book;
+    cout << "Input your name, surname, a book that you would like to recommend, as well as a political party you are planning to vote for" << "\n";
+    string name, surname, book, party;
+    cin >> name >> surname >> book >> party;
 
     ofstream f("info.txt");
-    f << name << " " << surname << " " << book << "\n";
+    f << name << " " << surname << " " << book << " " << party << "\n";
 }
 
 
