760e600eb75280d0f3ef6fecd668534129f10c11
Timmm005 <66874161+Timmm005@users.noreply.github.com>
2022-09-17 21:29:34 +0300

    added function printInfo which prints saved data in info.txt

--- main.cpp
+++ main.cpp
@@ -3,16 +3,36 @@
 using namespace std;
 
 
-int main()
+void printInfo()
+{
+    ifstream f("info.txt");
+    string line;
+
+    while (getline(f, line))
+    {
+        cout << line << "\n";
+    }
+    f.close();
+}
+
+
+
+void readInfo()
 {
     cout << "Input your name, surname and a book that you would like to recommend" << "\n";
     string name, surname, book;
     cin >> name >> surname >> book;
 
-    ofstream f("info.txt");
-    f << name << " " << surname << " " << book << "\n";
+    ofstream f;
+    f.open("info.txt", ios_base::app);
+
+    f << "\n" << name << " " << surname << " " << book;
+    f.close();
 }
 
 
-
-
+int main()
+{
+    readInfo();
+    //printInfo();
+}
