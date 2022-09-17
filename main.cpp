92a22ba30cc95f4b7c1d1ccba6f61ebabe05ac10
Timmm005 <66874161+Timmm005@users.noreply.github.com>
2022-09-17 21:33:08 +0300

    changed readInfo to ask questions from a file (questions.txt), made the file with 2q and made info.txt

--- main.cpp
+++ main.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
+
+
+void printInfo()
+{
+    ifstream f("info.txt");
+    string line;
+
+    while (getline(f, line))
+    {
+        cout << line << "\n";
+    }
+    f.close();
+}
+
+
+
+void readInfo()
+{
+    fstream q("questions.txt"), f;
+    f.open("info.txt", ios_base::app);
+    string questionLine, name, surname, answer, _;
+
+    cout << "Input your name and surname:\n";
+    cin >> name >> surname;
+    f << name << ", " << surname;
+    getline(cin, _);
+
+    cout << "Please answer the following questions:\n";
+    while (getline(q, questionLine))
+    {
+        cout << questionLine << "\n";
+        getline(cin, answer);
+        f << ", " << answer;
+    }
+
+    f << ";\n";
+
+
+    q.close();
+    f.close();
+}
 
 
 int main()
 {
-    cout << "Input your name, surname and a book that you would like to recommend" << "\n";
-    string name, surname, book;
-    cin >> name >> surname >> book;
-
-    ofstream f("info.txt");
-    f << name << " " << surname << " " << book << "\n";
+    readInfo();
+    //printInfo();
 }
 
 
